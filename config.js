baf545c20858e05ec361de03e87b601cc9157347
Sam Kimball <samuel.kimball@abarcahealth.com>
2022-03-24 20:13:15 -0500

    Prep tokens with new names, update config.js

--- config.js
+++ config.js
@@ -1,8 +1,33 @@
+// const StyleDictionary = require("style-dictionary");
+
+// const addUnitTransform = (token) => {
+//   console.log(token);
+//   const { value } = token;
+//   let size = value + "px";
+
+//   return size;
+// };
+
 module.exports = {
   source: [`tokens/**/*.json`],
+  // transform: {
+  //   addUnitTransform: {
+  //     type: "value",
+  //     transitive: "true",
+  //     matcher: (token) => token.attributes.category === "fontSizes",
+  //     transformer: addUnitTransform,
+  //   },
+  // },
   platforms: {
     css: {
-      transformGroup: "css",
+      transforms: [
+        "attribute/cti",
+        "name/cti/snake",
+        "time/seconds",
+        "content/icon",
+        "size/px",
+        "color/css",
+      ],
       buildPath: "build/css/",
       files: [
         {
